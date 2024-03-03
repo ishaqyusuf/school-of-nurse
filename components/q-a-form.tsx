daf0d3a1bb533ed363189a0e4eb501402de1cbc2
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-03-03 18:02:12 +0100

    :tada:

--- components/q-a-form.tsx
+++ components/q-a-form.tsx
@@ -2,9 +2,15 @@
 
 import { FormProvider } from "@/lib/provider";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import { useForm } from "react-hook-form";
 
 export default function QAFormSection() {
-  const value = {};
+  const form = useForm({
+    defaultValues: {},
+  });
+  const value = {
+    form,
+  };
   return (
     <FormProvider.Provider value={value}>
       <Tabs>
---
+++ lib/transformer.ts
@@ -0,0 +1,62 @@
+export function transformQuestions(questions: any, answers: any) {
+  let questionList: any[] = [];
+  let q: any = null;
+  let qs = questions
+    .split("\n")
+    .filter((f) => !Number(f.trim()))
+    .join(" ")
+    .replace("  ", " ");
+  Array(99)
+    .fill(null)
+    .map((a, i) => (qs = qs.replaceAll(` ${100 - i}. `, `\n${100 - i}. `)));
+  ["a", "b", "c", "d"].map((c) => {
+    qs = qs.replaceAll(`(${c}) `, `\n(${c}) `);
+  });
+  //   console.log(qs);
+  let answerrs = answers.replaceAll(`\n`, " ");
+  console.log(answerrs);
+  const answersSn: any = {};
+  Array(100)
+    .fill(null)
+    .map((a, i) => {
+      let sn = `${i + 1}.`;
+      let sec = answerrs.split(sn)[1]?.trim().split(" ")?.filter(Boolean)?.[0];
+      answersSn[sn] = sec;
+    });
+
+  let question: any = null;
+  console.log(qs.split("\n"));
+  let qss: any[] = [];
+  qs.split("\n").map((line) => {
+    line = line.trim();
+
+    let sn = line.split(".").filter(Boolean)[0];
+    //   console.log(sn);
+
+    if (Number(sn) > 0) {
+      if (question) {
+        qss.push({ ...question });
+        console.log(qss);
+      }
+      question = {
+        sn,
+        snDot: `${sn}.`,
+        question: line.replace(`${sn}. `, ""),
+        options: {},
+      };
+      return;
+    }
+    if (question) {
+      if (["(", ")"].every((s) => line.includes(s)) && line.startsWith("(")) {
+        let optnText = line.split(" ").filter(Boolean)[0];
+        if (optnText) {
+          let opt = line.replace(`${optnText} `, "");
+          question.options[optnText?.replace("(", "").replace(")", "")] = opt;
+          question.answer = answersSn[question.snDot];
+        }
+      }
+    }
+  });
+  if (question) qss.push(question);
+  console.log(question);
+}
